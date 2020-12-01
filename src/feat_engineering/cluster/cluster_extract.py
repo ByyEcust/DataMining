d8aca4e57f3120ca0a36d66e7d75320010c2e96c
Byy <1291024389@qq.com>
2020-12-01 21:15:51 +0800

    doc: add some comments

--- src/feat_engineering/cluster/cluster_extract.py
+++ src/feat_engineering/cluster/cluster_extract.py
@@ -6,17 +6,22 @@ class ClusterExtract(FeatEngineeringExtract):
     def __init__(self, agent_type, **init_params):
         self.agent = agent_type(**init_params)
 
-    def fit(self, feat, label=None, **fit_params):
-        self.agent.fit(feat, label, **fit_params)
+    def fit(self, feat, **fit_params):
+        """
+        To fit the model
+        :param feat: features in the form of np.array or pd.DataFrame
+        :param fit_params: those parameters that are required by agent
+        :return:
+        """
+        self.agent.fit(feat, **fit_params)
         return self
 
     def transform(self, feat):
         """
         transform the feat into cluster number encoding with one-hot form
         :param feat: np.array or pd.DataFrame, features in table forms
-        :return: one-hot features that indicating which cluster the sample belongs (without columns)
+        :return: one-hot features that indicating which cluster the sample belongs (without columns names)
         """
-
         labels = self.agent.predict(feat)
         cluster_feat = pd.get_dummies(labels)
         if type(feat) == pd.DataFrame:
