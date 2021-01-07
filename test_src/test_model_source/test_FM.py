e5fc264f09155258681362150d352d47913fc696
Ruoqiu Zhang <38268037+zrq339@users.noreply.github.com>
2021-01-07 10:25:54 +0800

    add prediction part to in the file of test_FM.py

--- test_src/test_model_source/test_FM.py
+++ test_src/test_model_source/test_FM.py
@@ -9,6 +9,7 @@ import pandas as pd
 import torch.nn as nn
 from src.model_source.FM import LR, FM, FNN
 from sklearn import datasets
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -26,19 +27,27 @@ x_valid = scaler.transform(x_valid)
 
 # model definition
 lr_model_ = LR(dim_features=x_train.shape[1])
-fm_model_ = FM(dim_features=x_train.shape[1], dim_embedding=4)
+fm_model_ = FM(dim_features=x_train.shape[1], dim_embedding=1)
 
 # training framework definition
 lr_model = FNN(model=lr_model_, loss_fn=nn.BCELoss())
 fm_model = FNN(model=fm_model_, loss_fn=nn.BCELoss())
 
 # training
-# lr_model.fit(x_train=x_train, y_train=y_train,
-#              x_valid=x_valid, y_valid=y_valid)
 
+lr_model.fit(x_train=x_train, y_train=y_train,
+             x_valid=x_valid, y_valid=y_valid,
+             lr=1e-1, batch_size=256)
+
+"""
 fm_model.fit(x_train=x_train, y_train=y_train,
              x_valid=x_valid, y_valid=y_valid,
-             lr=1e-2, batch_size=256)
+             lr=0.1, batch_size=256)
+"""
 
-
+# prediction
+y_train_pre = lr_model.predict(x_train)
+y_valid_pre = lr_model.predict(x_valid)
+print('the AUC of train: %f' %(roc_auc_score(y_train, y_train_pre)))
+print('the AUC of test: %f' %(roc_auc_score(y_valid, y_valid_pre)))
 
